9405dfcc195ab9b072b986a604975857441480c7
Dimash <dinmukhamed2517@gmail.com>
2023-10-01 11:55:02 +0600

    Committed

---
+++ app/src/main/java/kz/just_code/vehicle/MainActivity.kt
@@ -0,0 +1,14 @@
+package kz.just_code.vehicle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kz.just_code.vehicle.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
